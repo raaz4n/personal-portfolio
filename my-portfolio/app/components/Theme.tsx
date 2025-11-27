551198b6dd736abfa3282c1cde6a9165b2c7c4c3
raaz4n <raazandoski@gmail.com>
2025-11-26 21:34:12 -0600

    implemented theme button

---
+++ my-portfolio/app/components/Theme.tsx
@@ -0,0 +1,12 @@
+const ThemeButton = () => {
+  return (
+    <button className="flex flex-row items-center justify-center
+    pt-[9px] pl-[15px] pb-[9px] pr-[15px] h-[7%] 
+    absolute top-[200px] left-[81.5%] text-[50px] leading-[20px] 
+    text-(--nxt-clr) bg-(--nxt-bg) cursor-pointer">
+        ▧
+    </button>
+  )
+}
+
+export default ThemeButton
--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,28 +1,31 @@
+import ThemeButton from "./components/Theme";
+
 export default function Home() {
   return (
-    <section className={`flex flex-col items-left justify-center min-h-screen bg-(--color-background)`}>
+    <section className={`flex flex-col justify-center min-h-screen bg-(--color-background)`}>
         <div className={`block 
             ml-[3%] mb-[3%] mr-[3%] w-[94%] h-[83%] 
             bottom-[0%] absolute`}>
                 
         </div>
+        <ThemeButton></ThemeButton>
         <div className={`block 
             w-[500px] h-[300px] ml-[3%] mt-[50px] mr-[50px] mb-[50px] 
             pt-[30px] pb-[30px] absolute 
             bg(--color-background)`}>
-                <p className="static block mb-[10px] text-(--tcl1) text-[30px] font-normal leading-[20px]">
+                <p className="static block mb-[10px] text-[30px] font-normal leading-[20px]">
                     hi, i'm
                 </p>
-                <h1 className="static block mt-[10px] mb-[10px] text-(--tcl1) text-[60px] font-semibold leading-[44px]">
+                <h1 className="static block mt-[10px] mb-[10px] text-[60px] font-semibold leading-[44px]">
                     raazan
                 </h1>
                 <p className="static block mt-[10px] pt-[5px] mb-[10px] font-medium text-[20px] leading-[20px] text-(--tcl3) italic">
                     building apps, bots, and a 2D game.
                 </p>
-                <p className="static block pt-[60px] mb-[10px] font-normal text-[30px] leading-[20px] text-(--tcl1)">
+                <p className="static block pt-[60px] mb-[10px] font-normal text-[30px] leading-[20px]">
                     small developer • cs @ mtsu
                 </p>
-                <p className="font-secondary static flex flex-row items-left pt-[15px] mb-[10px] font-medium text-[20px] leading-[20px] text-(--tcl2)">
+                <p className="font-secondary static flex flex-row pt-[15px] mb-[10px] font-medium text-[20px] leading-[20px] text-(--tcl2)">
                     <a href="https://github.com/raaz4n">github</a>   ❘ ✦ ❘   <a href="https://linkedin.com/in/raazan">linkedin</a>
                 </p>
         </div>
