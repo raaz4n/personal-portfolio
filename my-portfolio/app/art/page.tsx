7b23066dfddf32e3c83091f2aa79527df4fb091e
raaz4n <raazandoski@gmail.com>
2025-11-27 15:55:18 -0600

    creating art page

--- my-portfolio/app/art/page.tsx
+++ my-portfolio/app/art/page.tsx
@@ -2,8 +2,21 @@ import ThemeButton from "../components/Theme";
 
 export default function art() {
   return (
-    <div>
-      
+    <div className={`flex flex-col justify-center items-center min-h-screen bg-(--background)`}>
+      <ThemeButton/>
+      <div className={`flex flex-col justify-start items-start gap-[34px] pt-[200px] min-h-screen w-[70%] static`}>
+        <div className={`flex flex-col justify-start items-start gap-[30px] mb-[60px] w-[100%] h-[15%]`}>
+            <h1 className={`mb-[10px] font-semibold text-[38px] leading-[44px] text-(--tcl1)`}>
+                Art
+            </h1>
+            <p className={`mb-[10px] font-normal text-[18px] leading-[20px] text-(--tcl1)`}>
+                A collection of 2D pixel art pieces, exploring environments, and small visual experiments.
+            </p>
+            <p className={`mb-[10px] font-normal text-[14px] leading-[20px] text-(--tcl2)`}>
+                ◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈
+            </p>
+        </div>
+      </div>
     </div>
   );
 }
--- my-portfolio/app/components/Navbar.tsx
+++ my-portfolio/app/components/Navbar.tsx
@@ -17,17 +17,17 @@ const Navbar = () => {
             </p>
         </Link>
         <Link href="/art" prefetch className={`flex flex-col justify-center items-center bg-(--at) cursor-pointer w-[20%]`}>
-            <p className={`block font-medium text-[40px] leading-[20px] text-white`}>
+            <p className={`block font-medium text-[40px] leading-[20px] text-[#333]`}>
                 art
             </p>
         </Link>
         <Link href="/blog" prefetch className={`flex flex-col justify-center items-center bg-(--bt) cursor-pointer w-[20%]`}>
-            <p className={`block font-medium text-[40px] leading-[20px] text-white`}>
+            <p className={`block font-medium text-[40px] leading-[20px] text-[#333]`}>
                 blog
             </p>
         </Link>
         <Link href="/projects" prefetch className={`flex flex-col justify-center items-center bg-(--pt) cursor-pointer w-[20%]`}>
-            <p className={`block font-medium text-[40px] leading-[20px] text-white`}>
+            <p className={`block font-medium text-[40px] leading-[20px] text-[#333]`}>
                 projects
             </p>
         </Link>
