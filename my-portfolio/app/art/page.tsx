3d3a4d56f97553d2ac0c6584ee1f35ccac84b594
raaz4n <raazandoski@gmail.com>
2025-11-27 23:18:40 -0600

    art page finish

--- my-portfolio/app/art/page.tsx
+++ my-portfolio/app/art/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 import ThemeButton from "../components/Theme";
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
+// @ts-ignore
+import PhotoSwipeDynamicCaption from 'photoswipe-dynamic-caption-plugin';
 import 'photoswipe/style.css';
+import 'photoswipe-dynamic-caption-plugin/photoswipe-dynamic-caption-plugin.css';
 import PhotoSwipe from 'photoswipe';
 
 export default function art() {
@@ -12,14 +15,24 @@ export default function art() {
             children: 'a',
             pswpModule: PhotoSwipe,
             showHideAnimationType: 'none',
+            loop: false,
+            imageClickAction: 'zoom',
+            doubleTapAction: 'zoom',
+            maxWidthToAnimate: 0,
+            initialZoomLevel: 'fit',
+            secondaryZoomLevel: 2,
         });
+
+        const captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
+            type: 'aside',
+        });
+
         lightbox.init();
 
         return() => {
             lightbox.destroy();
         };
     }, []);
-
 
   return (
     <div className={`flex flex-col justify-center items-center min-h-screen bg-(--background)`}>
@@ -36,17 +49,49 @@ export default function art() {
                 ◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈◈
             </p>
         </div>
-        <div id="gallery" className={`grid grid-cols-3 gap-y-4 gap-x-20 w-full pl-[10px] pr-[10px]`}>
-            <a href="/file.svg" 
-            data-pswp-width="200"
-            data-pswp-height="200"
-            target="_blank" 
-            rel="norefferer" 
-            className='border border-pink-400 rounded-lg shadow-xl min-h-[400px]'>
-                <img src="/file.svg" className="w-full h-full object-cover"/>
-            </a>
-            <div className='border border-pink-400 rounded-lg shadow-xl min-h-[400px]'/>
-            <div className='border border-pink-400 rounded-lg shadow-xl min-h-[400px]'/>
+        <div className={`grid grid-cols-3 gap-y-4 gap-x-20 w-full pl-[10px] pr-[10px]`}>
+            <div className={`border border-pink-400 rounded-lg shadow-xl min-h-[490px]`}>
+                <div id = "gallery">
+                    <a href="/hoshi/hoshilogo.png"
+                    data-pswp-width="650"
+                    data-pswp-height="650">
+                        <img src="/hoshi/hoshilogo.png" className="rounded-t-lg w-[400px] h-[400px]"/>
+                        <span className="pswp-caption-content">The logo for my 2D space-adventure game known as Hoshi.
+                                                               This logo represents the main character - Hoshi, as well as all of the worlds that she explores.
+                                                               It is also known as the Kanji 星, which translates to "star".
+                        </span>
+                    </a>
+                    <a href="/hoshi/spi2.png"
+                    data-pswp-width="650"
+                    data-pswp-height="850">
+                        <span className="pswp-caption-content">This image represents one of the many worlds that Hoshi will explore.
+                                                               The planet known as Spî is a large and deserted planet with oddities like none other.
+                                                               Covered by vast white sand and oceans, this world can cause a feeling of unease.
+                        </span>
+                    </a>
+                    <a href="/hoshi/world.png"
+                    data-pswp-width="650"
+                    data-pswp-height="650">
+                        <span className="pswp-caption-content">This is the first portion of the game, including 3 planets and a sort of space hub.
+                        </span>
+                    </a>
+                </div>
+                <div className={`flex flex-col justify-center items-center w-full h-[85px] rounded-b-lg bg-(--background)`}>
+                    <p className={`text-(--tcl1) text-[25px]`}>
+                        Hoshi
+                    </p>
+                    <p className={`text-(--tcl2) text-[19px]`}>
+                        2025-present
+                    </p>
+                </div>
+            </div>
+            <div className={`border border-pink-400 rounded-lg shadow-xl min-h-[400px]`}>
+                <div className={`flex flex-col justify-center items-center h-[29em]`}>
+                    <h1 className={`font-secondary text-(--tcl2) text-[40px]`}>
+                        More to come soon!
+                    </h1>
+                </div>
+            </div>
         </div>
       </div>
     </div>
